476b9fc6fc209463564e28b3c4b4ef3dd47af32f
Eder Díaz <ederchrono@gmail.com>
2020-06-14 17:32:40 -0500

    :fire: remove cors issues with lambdas

--- lambda/testimonials.js
+++ lambda/testimonials.js
@@ -1,6 +1,13 @@
 module.exports.handler = async function (event, context) {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+  }
+
   return {
     statusCode: 200,
+    headers,
     body: JSON.stringify([
       {
         id: 1,
